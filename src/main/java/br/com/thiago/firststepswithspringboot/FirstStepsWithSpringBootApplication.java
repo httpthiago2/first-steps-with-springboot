a9b5d7065bd44b64f981ea3fbd90cc21e62bb571
thiago.gonzalez <thiagogonzalez.dev@gmail.com>
2023-10-23 17:04:31 -0300

    feat: my first controller

---
+++ src/main/java/br/com/thiago/firststepswithspringboot/FirstStepsWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.firststepswithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepsWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStepsWithSpringBootApplication.class, args);
+	}
+
+}
