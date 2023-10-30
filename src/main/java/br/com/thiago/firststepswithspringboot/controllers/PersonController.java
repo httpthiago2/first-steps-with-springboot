8aa67bfaa802d004edd5659a6603ce1a0ea3e32b
thiago.gonzalez <thiagogonzalez.dev@gmail.com>
2023-10-30 16:31:41 -0300

    refact: refactoring person service and controller for the repository.

--- src/main/java/br/com/thiago/firststepswithspringboot/controllers/PersonController.java
+++ src/main/java/br/com/thiago/firststepswithspringboot/controllers/PersonController.java
@@ -19,7 +19,7 @@ public class PersonController {
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person findById(@PathVariable(value = "id") String id) {
+    public Person findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
@@ -53,7 +53,7 @@ public class PersonController {
             value = "/{id}",
             method = RequestMethod.DELETE
     )
-    public void deleteById(@PathVariable(value = "id") String id) {
+    public void deleteById(@PathVariable(value = "id") Long id) {
         service.delete(id);
     }
 
--- src/main/java/br/com/thiago/firststepswithspringboot/service/PersonService.java
+++ src/main/java/br/com/thiago/firststepswithspringboot/service/PersonService.java
@@ -1,46 +1,52 @@
 package br.com.thiago.firststepswithspringboot.service;
 
 import br.com.thiago.firststepswithspringboot.entity.Person;
-import br.com.thiago.firststepswithspringboot.utils.MockUtils;
+import br.com.thiago.firststepswithspringboot.exceptions.ResourceNotFoundException;
+import br.com.thiago.firststepswithspringboot.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonService {
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
-    public Person findById(String id) {
-        logger.info("###Finding one person");
+    @Autowired
+    private PersonRepository repository;
 
-        Person person = MockUtils.mockPerson(1);
-        return person;
+    public Person findById(Long id) {
+        logger.info("###Finding one person");
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
     }
 
     public List<Person> findAll() {
         logger.info("###Finding all persons");
-        List<Person> personList = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            Person person = MockUtils.mockPerson(i);
-            personList.add(person);
-        }
-        return personList;
+        return repository.findAll();
     }
 
     public Person create(Person person) {
         logger.info("###Creating a person");
-        return person;
+        return repository.save(person);
     }
 
     public Person update(Person person) {
         logger.info("###Updating a person with id: " + person.getId());
-        return person;
+
+        var entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(entity);
     }
 
-    public void delete(String id) {
+    public void delete(Long id) {
+        var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
         logger.info("###Deleting person with id: " + id);
+        repository.delete(entity);
     }
 }
